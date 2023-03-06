a38cd1f51b5cc771921e1acd8eac6d676c1a4305
Mathilde Larret-Mary <mathilde.larret.dev@gmail.com>
2023-03-06 17:44:17 +0100

    responsive header

--- dist/fetches.js
+++ dist/fetches.js
@@ -1,5 +1,5 @@
+"use strict";
 /* API : 'https://pokebuildapi.fr/api/v1'*/
 // TODO: get all pkm names and put in on an object to search by name
 // TODO: get all pkms to an object to create all cards
-export const ligne = "lol";
-//# sourceMappingURL=request.js.map
+//# sourceMappingURL=fetches.js.map
--- src/request.ts
+++ src/request.ts
@@ -1,3 +1,4 @@
 /* API : 'https://pokebuildapi.fr/api/v1'*/
-// TODO: get all pkm names and put in on an object
+// TODO: get all pkm names and put in on an object to search by name
+// TODO: get all pkms to an object to create all cards
 export const ligne:string = "lol"
