9662d59e2050a13ca07d9402a89123ce8ec8e61e
Mathilde Larret-Mary <mathilde.larret.dev@gmail.com>
2023-03-29 14:22:49 +0200

    WebComponent PokeCard DOM Encoded

--- dist/pokeCard.js
+++ dist/pokeCard.js
@@ -4,6 +4,99 @@ class PokeCard extends HTMLElement {
         super();
         this._root = _root;
         this._root = this.attachShadow({ mode: 'open' });
+        this._root.innerHTML = `
+        <style>
+        .card, .resist-card {
+            font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+            position: relative;
+            background-color: var(--light-medium-color);
+            border-radius: 20px;
+            border: var(--dark-color) 4px solid;
+            min-width: 250px;
+            height: 350px;
+            /*TODO: change on @media-queries */
+            margin: 15px 0;
+        
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-around;
+        
+            box-shadow: 3px 8px 7px 2px rgba(0,0,0,0.5);
+        }
+        
+        .card {
+            max-width: 250px;
+        }
+        
+        .card-img {
+            background-color: var(--medium-color);
+            border: var(--dark-color) 3px solid;
+            border-radius: 20px;
+            margin: 10px auto 5px;
+            width: 90%;
+            height: 40%;
+            object-fit: contain;
+        }
+        
+        .card-name {
+           text-transform: capitalize;
+           color: var(--light-color);
+            font-weight: 700;
+            background-color: var(--dark-color);
+            border-radius: 30px;
+            padding: 3px 5px 5px 5px;
+            margin: 5px 5px 5px 10px;
+            position: absolute;
+            left: 0;
+        }
+        
+        .card-gen {
+            font-weight: 300;
+            font-size: smaller;
+            background-color: var(--dark-medium-color);
+            position: absolute;
+            padding: 3px 7px 3px 5px;
+            margin: 0 10px 8px;
+            border-radius: 10px;
+            bottom: 57%;
+            right: 10px;
+        }
+        
+        .card-stats {
+            background-color: var(--medium-color);
+            border: var(--dark-medium-color) 3px solid;
+            border-radius: 20px;
+            margin: 5px auto 10px;
+            width: 90%;
+            min-height: 50%;
+        
+            display: flex;
+            flex-wrap: wrap;
+        }
+        
+        .card-stat {
+            background-color: var(--dark-medium-color);
+            border-radius: 20px;
+            width: 80%;
+            margin: 2px auto;
+            padding: 4px 10px 0;
+        }
+        
+        .more {
+            background-color: var(--light-color);
+            color: var(--dark-color);
+            border: var(--dark-color) solid 2px;
+            position: absolute;
+            right: 0;
+            height: 30px;
+            width: 30px;
+            padding: 1px 1px 0 0;
+            border-radius: 30px;
+            font-weight: bolder;
+            font-size: large;
+        }
+        </style>
+        `;
     }
     connectedCallback() {
         const _data_generation = this.getAttribute('data-generation');
@@ -16,24 +109,57 @@ class PokeCard extends HTMLElement {
         const _data_stat_att_spe = this.getAttribute('data-stat-att-spe');
         const _data_stat_def_spe = this.getAttribute('data-stat-def-spe');
         const _data_stat_speed = this.getAttribute('data-stat-speed');
-        this._root.innerHTML = `
-        <h2 class="card-gen">Gen : ${_data_generation}</h2>
-        <img src="${_data_sprite}" class="card-img">
-        <span class="card-name">${_data_name}</span><button value = "${_data_id}" class="more">+</button>
-        <div class="card-stats">
-            <span class="card-stat">HP : ${_data_stat_hp}</span>
-            <span class="card-stat">Att : ${_data_stat_att}</span>
-            <span class="card-stat">Def : ${_data_stat_def}</span>
-            <span class="card-stat"> Att.Spé : ${_data_stat_att_spe}</span>
-            <span class="card-stat">Def.Spé : ${_data_stat_def_spe}</span>
-            <span class="card-stat">Vit : ${_data_stat_speed}</span>
-        </div>
-        `;
+        this.buildDOM(_data_generation, _data_sprite, _data_name, _data_id, _data_stat_hp, _data_stat_att, _data_stat_def, _data_stat_att_spe, _data_stat_def_spe, _data_stat_speed);
     }
     buildDOM(gen, sprite, name, id, stat_hp, stat_att, stat_def, stat_att_spe, stat_def_spe, stat_speed) {
+        const cardDiv = document.createElement('div');
+        cardDiv.classList.add('card');
         const title = document.createElement('h2'); // _data_name
+        title.classList.add('card-name');
+        title.textContent = name;
+        cardDiv.append(title);
         const generation = document.createElement('span'); // _data_generation
+        generation.classList.add('card-gen');
+        generation.textContent = 'Gen : ' + gen;
+        cardDiv.append(generation);
         const image = document.createElement('img'); // _data_sprite
+        image.classList.add('card-img');
+        image.src = sprite;
+        cardDiv.append(image);
+        const moreBtn = document.createElement('button'); // _data_id
+        moreBtn.classList.add('more');
+        moreBtn.value = id;
+        moreBtn.textContent = '+';
+        cardDiv.append(moreBtn);
+        // stats--------------------------------------------------------------------------
+        const stats = document.createElement('div');
+        stats.classList.add('card-stats');
+        const statHp = document.createElement('span'); // stat_hp
+        statHp.classList.add('card-stat');
+        statHp.textContent = 'HP : ' + stat_hp;
+        stats.append(statHp);
+        const statAtt = document.createElement('span'); // stat_att
+        statAtt.classList.add('card-stat');
+        statAtt.textContent = 'Att : ' + stat_att;
+        stats.append(statAtt);
+        const statDef = document.createElement('span'); // stat_def
+        statDef.classList.add('card-stat');
+        statDef.textContent = 'Def : ' + stat_def;
+        stats.append(statDef);
+        const statAttSpe = document.createElement('span'); // stat_att_spe
+        statAttSpe.classList.add('card-stat');
+        statAttSpe.textContent = 'Att.Spé : ' + stat_att_spe;
+        stats.append(statAttSpe);
+        const statDefSpe = document.createElement('span'); // stat_def_spe
+        statDefSpe.classList.add('card-stat');
+        statDefSpe.textContent = 'Def.Spé : ' + stat_def_spe;
+        stats.append(statDefSpe);
+        const statSpeed = document.createElement('span'); // stat_speed
+        statSpeed.classList.add('card-stat');
+        statSpeed.textContent = 'Vit : ' + stat_speed;
+        stats.append(statSpeed);
+        cardDiv.append(stats);
+        this._root.append(cardDiv);
     }
 }
 const App = document.querySelector('#app');
